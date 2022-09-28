cab633a491601bbe5bf633bb41f8465d9b9b8e4f
Michiel Sikma <michiel@sikma.org>
2022-09-28 23:42:50 +0200

    Add TTS placeholder

---
+++ packages/zerglingbot-core/actions/redemptions/tts.js
@@ -0,0 +1,26 @@
+// zerglingbot <https://github.com/msikma/zerglingbot>
+// © MIT license
+
+/**
+ * Redemption for speaking a TTS message.
+ * 
+ * Note that this is currently not being used.
+ * Why not? Because we can't quite filter out the user's emoji
+ * from their message very easily. So instead, we handle the
+ * redemptions on the client side in the chat code.
+ * 
+ * See the chat code in Stream Resources.
+ */
+// const tts = {
+//   name: 'tts',
+//   rewardIDs: {
+//     '4d8503fa-19be-434f-8198-dea2b30b9bff': 'speakMyMessage',
+//   },
+//   help: `Generates a TTS message and plays it back on the stream.`,
+//   action: async ({apiClient, chatClient}, type, msg, config, msgObject) => {
+//   }
+// }
+
+// module.exports = {
+//   tts
+// }
