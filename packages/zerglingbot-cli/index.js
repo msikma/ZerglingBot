eace49d54741e586e1b7a47f91c1f3ae74ef8433
Michiel Sikma <michiel@sikma.org>
2022-10-04 17:43:10 +0200

    Clean up help output

--- packages/zerglingbot-cli/index.js
+++ packages/zerglingbot-cli/index.js
@@ -21,11 +21,11 @@ const main = async () => {
   cliParser.addArgument(['--log-dates'], {help: 'Includes dates in the logger (for cron).', dest: 'includeDates', action: 'storeTrue'})
   cliParser.addArgument(['--restart'], {help: 'Request the active instance to exit.', dest: 'requestRestart', action: 'storeTrue'})
   
-  cliParser.addArgument(['--path-ffmpeg'], {help: 'Path to the ffmpeg binary.', dest: 'pathFFMPEG', defaultValue: `/usr/local/bin/ffmpeg`})
-  cliParser.addArgument(['--path-ffprobe'], {help: 'Path to the ffprobe binary.', dest: 'pathFFProbe', defaultValue: `/usr/local/bin/ffprobe`})
-  cliParser.addArgument(['--path-say'], {help: 'Path to the say binary.', dest: 'pathSay', defaultValue: `/usr/bin/say`})
-  cliParser.addArgument(['--path-node'], {help: 'Path to the Node binary.', dest: 'pathNode', defaultValue: `/usr/local/bin/node`})
-  cliParser.addArgument(['--path-bnetdata'], {help: 'Path to the bnetdata binary.', dest: 'pathBnetdata', defaultValue: `${os.homedir()}/Utilities/bnetdata/index.js`})
+  cliParser.addArgument(['--path-ffmpeg'], {help: 'Path to the ffmpeg binary.', dest: 'pathFFMPEG', metavar: 'PATH', defaultValue: `/usr/local/bin/ffmpeg`})
+  cliParser.addArgument(['--path-ffprobe'], {help: 'Path to the ffprobe binary.', dest: 'pathFFProbe', metavar: 'PATH', defaultValue: `/usr/local/bin/ffprobe`})
+  cliParser.addArgument(['--path-say'], {help: 'Path to the say binary.', dest: 'pathSay', metavar: 'PATH', defaultValue: `/usr/bin/say`})
+  cliParser.addArgument(['--path-node'], {help: 'Path to the Node binary.', dest: 'pathNode', metavar: 'PATH', defaultValue: `/usr/local/bin/node`})
+  cliParser.addArgument(['--path-bnetdata'], {help: 'Path to the bnetdata binary.', dest: 'pathBnetdata', metavar: 'PATH', defaultValue: `${os.homedir()}/Utilities/bnetdata/index.js`})
   
   // Parse command line arguments; if something is wrong, the program exits here.
   const args = {...cliParser.parseArgs(), pathPackage: path.resolve(path.dirname(pkgPath)), packageData: pkgData}
