9c277885c1c6c447ac2ae1eee22a5bf7325d874c
Michiel Sikma <michiel@sikma.org>
2023-11-20 20:07:59 +0100

    Remove streaminfo cron task in favor of the stream_info broadcaster

--- packages/zerglingbot-core/lib/cron/tasks/index.js
+++ packages/zerglingbot-core/lib/cron/tasks/index.js
@@ -4,7 +4,6 @@
 module.exports = {
   ...require('./announcements'),
   ...require('./dosbox'),
-  ...require('./streaminfo'),
   ...require('./ladderinfo'),
   ...require('./winampnp'),
   ...require('./webcam')
